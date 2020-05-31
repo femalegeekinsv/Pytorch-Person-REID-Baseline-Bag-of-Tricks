d093c23cf38e9718fbb5b22b71b22e2518c50de2
yumworm <42192687+yumworm@users.noreply.github.com>
2020-05-30 18:34:07 -0700

    Update visualize.py

--- core/visualize.py
+++ core/visualize.py
@@ -3,6 +3,52 @@ from tools import CatMeter, cosine_dist, visualize_ranked_results
 import time
 import glob
 
+def inference_buildgallery(config, base, loaders):
+
+	base.set_eval()
+				
+	dir_q = '/home/teresa/reID/temp'
+	q_list = sorted(glob.glob(dir_q + '/*'))
+	
+	for i, q in enumerate(q_list):
+		print('query', i, q)
+		loaders.set_query(q, 0, 1) #TODO: mod for multiple queries
+		loaders.process_query_data()
+		features_g = None
+		
+		with torch.no_grad():	
+			for batch_num, data in enumerate(loaders.query_loader):			
+				images, pids, cids = data			
+				features_q = base.model(images) #can be multiple features				
+				if len(loaders.gallery) > 0:
+					for g in loaders.gallery:
+						path, feat, pid, cid = g
+						if features_g is None:
+							features_g = feat.unsqueeze(0)
+						else:
+							features_g = torch.cat([features_g, feat.unsqueeze(0)], dim=0)
+					#extract feat from tuples and convert to torch
+					#print(features_g)
+					distance = cosine_dist(features_q, features_g).data.cpu().numpy()
+					print(distance)
+					indices = np.argsort(distance, axis=1)[:,::-1] #smallest distance first, reverse order when add to gallery
+					print(indices)
+					closest_dist = distance[0][indices[0][0]]
+					print('closest match with dist',loaders.gallery[indices[0][0]][0], distance[0][indices[0][0]])
+					if closest_dist > 0.6:
+						pids = [loaders.gallery[indices[0][0]][2]] #2 for pid
+					else:
+						pids = [i]
+					#pids = assign_pid(distance,loaders.gallery) #one query has one row of distance hence 
+					
+					#another function to sort with index and assign PID
+				#for f in features_q: #TODO: mod for muliple queries
+				else:
+					pids = [i] #first pid for empty gallery
+				for fp in zip(features_q,pids):
+					loaders.add_gallery_all(q, fp[0], fp[1], 1)	
+	print(loaders.gallery)
+
 def visualize(config, base, loaders):
 
 	base.set_eval()
